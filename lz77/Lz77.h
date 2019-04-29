0cbc9c75a0723be2304b246b1469f26844f2bcf1
philopateer nabil <41947911+phella@users.noreply.github.com>
2019-04-29 18:15:38 +0200

    Add files via upload

---
+++ lz77/Lz77.h
@@ -0,0 +1,59 @@
+﻿#ifndef _LZ77_H
+#define _LZ77_H
+
+#include<bitset>
+#include<vector>
+using namespace std;
+// Struct to hold the triplet for each entry
+struct Triplet{
+	// Starting position of the longest match in the previous string
+	// i.e. number of symbols to look back.
+	unsigned int j;
+
+	// Length of the longest match i.e. number of symbols to copy
+	// starting at the match position.
+	unsigned int k;
+
+	// Uncompressed code of the symbol following the match. The number
+	// of bits here will depend on the size of the alphabet in use.
+	unsigned char c;
+	Triplet(unsigned int start , unsigned int longest ,unsigned char code)
+	{
+		j = start;
+		k = longest;
+		c = code;
+	}
+};
+
+class LZ77 {
+public:
+	// Constructor and destructor
+
+	// A function to encode the input message using LZ77.
+	//
+	// Inputs:
+	// - the input message
+	// - S the size of the match buffer i.e. the maximum distance to look
+	//   backward for a match
+	// - T the size of the lookahead buffer
+	// - A the size of the alphabet
+	//
+	// The encoded message contains the triplets output from the input msg.
+	//
+	// The function should return the number of bits in the encoded
+	// message.
+	int encode(const vector<unsigned char>& msg,
+		int S, int T, int A,
+		vector<Triplet>* encoded_msg);
+
+	// A function to decode a message encoded using LZ77.
+	void decode(const vector<Triplet>& encoded_msg,
+		vector<unsigned char>* decoded_msg);
+
+
+private:
+	// Any data members here ...
+
+};
+
+#endif
---
+++ lz77/Source.cpp
@@ -0,0 +1,102 @@
+﻿#include"Lz77.h"
+#include<iostream>
+void readPgm(string& version,int& width ,int& height , int& range ,vector<unsigned char>* img );
+void readEncoded(vector<Triplet>* encodedMsg);
+int main(int argc, char* argv[]) {
+	// read parameters here
+	LZ77 lz77 ;
+	string command;
+	vector<Triplet> encodedMsg;
+	vector<unsigned char> message;
+	string version;
+	int width,height,range;
+	if(strncmp(argv[1], "-encode",7) == 0)
+	{
+		int searchBuff,lookAhead,symbols;
+		searchBuff=stoi(argv[2]);
+		lookAhead = stoi(argv[3]);
+		symbols = stoi(argv[4]);
+		readPgm(version,width,height,range,&message);
+		cout<<lz77.encode(message ,searchBuff,lookAhead,symbols,&encodedMsg);
+		cout<<encodedMsg.size()<<endl;
+		for(int i=0;i<encodedMsg.size();i++)
+		{
+			cout<<encodedMsg[i].j<<" "<<encodedMsg[i].k<<" "<<(int)encodedMsg[i].c<<endl;
+		}
+	}
+	else if(strncmp(argv[1],"-decode",7)==0)
+	{
+		int xres=stoi(argv[2]);
+		int yres=stoi(argv[3]);
+		int max = stoi(argv[4]);
+		vector<unsigned char> decodedMsg;
+		readEncoded(&encodedMsg);
+		lz77.decode(encodedMsg,&decodedMsg);
+		cout<<"P2"<<endl<<256<<" "<<256<<endl<<255<<endl;
+		for(int i=0;i<xres;i++)
+		{
+			for(int j=0;j<yres;j++)
+			{
+				cout<<(int)decodedMsg[i*yres+j]<<" ";
+			}
+		}
+	}
+	return 0;
+}
+
+
+
+void readPgm(string& version,int& width ,int& height , int& range ,vector<unsigned char>* img )
+{
+	char x;
+	string test;
+	int val;
+	cin>>x;
+	version+=x;
+	cin>>x;
+	version+=x;
+	while(true)
+	{
+		cin>>test;
+		if(test[0]!='#')
+		{
+			break;
+		}
+		else
+		{
+			getline(cin,test);
+		}
+	}
+	width = stoi(test);
+	cin>>height;
+	cin>>range;
+	for(int i=0;i<width*height;i++)
+	{
+		cin>>val;
+		img->push_back(val);
+	}
+}
+
+void readEncoded(vector<Triplet>* encodedMsg)
+{
+	long long size;
+	cin>>size;
+	int j,k,c;
+	for(int i=0;i<size;i++)
+	{
+		cin>>j>>k>>c;
+		Triplet add(j,k,(char)c);
+		encodedMsg->push_back(add);
+	}
+}
+
+void decorrelation(int width,int height,vector<unsigned char>*symbols)
+{
+	for(int i=0;i<height;i++)
+	{
+		for(int j=width-1;j>0;j--)
+		{
+			symbols->at(i*width+j) -= symbols->at(i*width+j-1) ;
+		}
+	}
+}
