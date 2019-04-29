a3b09fecfc71dadd76094897b069317408b74dd3
philopateer nabil <41947911+phella@users.noreply.github.com>
2019-04-29 23:46:34 +0200

    last version

---
+++ lz77/Lz77.cpp
@@ -0,0 +1,82 @@
+﻿#include"Lz77.h"
+#include<iostream>
+int LZ77::encode(const vector<unsigned char>& msg,int S, int T, int A,vector<Triplet>* encoded_msg)
+{
+	string message ="" ;
+	int maxSize =S+T;
+	string subs1,subs2;
+	for(int i =0;i<msg.size();i++)
+	{
+		message+=msg[i];
+	}
+
+	for(int i=0;i<msg.size();i++)
+	{
+		int added=0;
+		Triplet codeword2(0,0,msg[i]);
+		int start =  ((i-S > 0) ? i-S : 0);
+		int end =  ((i+T < msg.size()) ? i+T : msg.size());
+		bool flag=false;
+		for(int  j=start;j<end;j++)
+		{
+			if(flag)
+				break;
+			Triplet codeword(0,0,msg[i]) ;
+			bool found =false;
+			for(int k=0;k<maxSize && i+k+1 <msg.size();k++)
+			{
+				subs1 = message.substr(i,k+1);
+				subs2= message.substr(j,k+1);
+				if( i==j && codeword.k == 0)
+				{
+					flag= true;
+					break;
+				}
+				if(subs1==subs2)
+				{
+					found = true;
+					codeword.k = k+1;
+					codeword.j = i-j;
+					codeword.c =(int) message[i+k+1];
+					if(codeword.k > codeword2.k)
+					{
+						codeword2 = codeword;
+						if(codeword.k>0)
+							added = codeword.k;
+					}
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		encoded_msg->push_back(codeword2);
+		i+=added;
+	}
+	return encoded_msg->size()*(ceil( log(S)/log(2))+ ceil( log(S+T)/log(2)) +ceil( log(A)/log(2)));
+}
+
+void LZ77::decode(const vector<Triplet>& encoded_msg,vector<unsigned char>* decoded_msg)
+{
+	int decodedCounter=0;
+	for (int i =0;i<encoded_msg.size();i++)
+	{
+		if(encoded_msg[i].k==0)
+		{
+			decoded_msg->push_back((int)encoded_msg[i].c);
+			decodedCounter++;
+		}
+		else
+		{
+			int offset = encoded_msg[i].j;
+			for(int j=0;j<encoded_msg[i].k;j++)
+			{
+				decoded_msg->push_back((int)decoded_msg->at(decodedCounter-offset));
+				decodedCounter++;
+			}
+			decoded_msg->push_back((int)encoded_msg[i].c);
+			decodedCounter++;
+		}
+	}
+}
--- lz77/Source.cpp
+++ lz77/Source.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 void readPgm(string& version,int& width ,int& height , int& range ,vector<unsigned char>* img );
 void readEncoded(vector<Triplet>* encodedMsg);
+void decorrelation(int width,int height,vector<unsigned char>*symbols);
 int main(int argc, char* argv[]) {
 	// read parameters here
 	LZ77 lz77 ;
@@ -10,14 +11,14 @@ int main(int argc, char* argv[]) {
 	vector<unsigned char> message;
 	string version;
 	int width,height,range;
-	if(strncmp(argv[1], "-encode",7) == 0)
+	if(strncmp(argv[1], "-encode",7) == 0 &&strncmp(argv[2],"-diff",5)!=0)
 	{
 		int searchBuff,lookAhead,symbols;
 		searchBuff=stoi(argv[2]);
 		lookAhead = stoi(argv[3]);
 		symbols = stoi(argv[4]);
 		readPgm(version,width,height,range,&message);
-		cout<<lz77.encode(message ,searchBuff,lookAhead,symbols,&encodedMsg);
+		cout<<lz77.encode(message ,searchBuff,lookAhead,symbols,&encodedMsg)<<endl;
 		cout<<encodedMsg.size()<<endl;
 		for(int i=0;i<encodedMsg.size();i++)
 		{
@@ -26,19 +27,36 @@ int main(int argc, char* argv[]) {
 	}
 	else if(strncmp(argv[1],"-decode",7)==0)
 	{
+		int dummy;
+		cin>>dummy;
 		int xres=stoi(argv[2]);
 		int yres=stoi(argv[3]);
 		int max = stoi(argv[4]);
 		vector<unsigned char> decodedMsg;
 		readEncoded(&encodedMsg);
 		lz77.decode(encodedMsg,&decodedMsg);
-		cout<<"P2"<<endl<<256<<" "<<256<<endl<<255<<endl;
+		cout<<"P2"<<endl<<xres<<" "<<yres<<endl<<max<<endl;
 		for(int i=0;i<xres;i++)
 		{
 			for(int j=0;j<yres;j++)
 			{
 				cout<<(int)decodedMsg[i*yres+j]<<" ";
 			}
+		}
+	}
+	else
+	{
+		int searchBuff,lookAhead,symbols;
+		searchBuff=stoi(argv[3]);
+		lookAhead = stoi(argv[4]);
+		symbols = stoi(argv[5]);
+		readPgm(version,width,height,range,&message);
+		decorrelation(width,height,&message);
+		cout<<lz77.encode(message ,searchBuff,lookAhead,symbols,&encodedMsg)<<endl;
+		cout<<encodedMsg.size()<<endl;
+		for(int i=0;i<encodedMsg.size();i++)
+		{
+			cout<<encodedMsg[i].j<<" "<<encodedMsg[i].k<<" "<<(int)encodedMsg[i].c<<endl;
 		}
 	}
 	return 0;
