b04833eca5fc56f4699c7d3ffe3e84ea65781727
philopateer nabil <41947911+phella@users.noreply.github.com>
2019-04-29 23:55:57 +0200

    Add files via upload

---
+++ huffman.h
@@ -0,0 +1,66 @@
+﻿#ifndef _HUFFMAN_H
+#define _HUFFMAN_H
+
+#include <vector>
+#include<map>
+#include<string>
+using namespace std;
+// Define the tree node struct here ...
+struct Node {
+	Node(){
+		left=NULL;
+		right = NULL;
+	};
+	Node* left;
+	Node* right;
+	int value;
+	float probability;
+};
+// Define a symbol struct here to hold the symbol (grayscale value)
+// and its probability/frequency
+struct Symbol {
+	// Grayscale value of the symbol
+	unsigned char val;
+	float probability;
+	int frequency;
+};
+
+class Huffman {
+public:
+	Huffman(){};
+	// Constructor and destructor
+
+	// A function to compute the entropy of the input message and
+	// stores the probabilities of the different symbols in
+	// the input array.	s
+	float compute_entropy(const vector<unsigned char>& msg,
+		vector<Symbol>* prob);
+
+	// A function to build the Huffman tree given the computed
+	// symbol probabilities.
+	void build_tree(const vector<Symbol>& prob);
+
+	// A function to print the code table computed from the build_tree above.
+	void print_code_table();
+
+	// A function to encode the input message using the computed tree
+	// and store the output in the output array, where each entry
+	// corresponds to the codeword for an input symbol in the message.
+	//
+	// The function should return the number of bits in the encoded
+	// message.
+	int encode(const vector<unsigned char>& msg,
+		vector<unsigned int>* encoded_msg);
+	vector<Symbol>* getSymbols();
+	void clear();
+	void setCodewords(Node* root , string path);
+
+private:
+	// Data members here ... for example to store the tree and the codeword
+	// hash table
+	vector<Symbol> symbols;
+	Node* tree;
+	map<int,string> codewords;
+};
+
+#endif
---
+++ hw1_p1.cpp
@@ -0,0 +1,84 @@
+﻿#include "huffman.h"
+#include<iostream>
+#include<string>
+
+using namespace std;
+void decorrelation(int width,int height,vector<unsigned char>*symbols);
+void readPgm(string& version,int& width ,int& height , int& range ,vector<unsigned char>* img);
+int main(int argc, char* argv[])
+{
+	string version;
+	string commands[2];
+	int width,height,range;
+	vector<unsigned char> img;
+	vector<unsigned int> encodedMsg;
+	readPgm(version,width,height,range,&img);
+	Huffman huffman;
+	if( strncmp( argv[1], "-entropy",8)==0)
+	{
+		float entropy =huffman.compute_entropy(img,huffman.getSymbols());
+		cout<<entropy<<endl;
+	}
+	else if (strncmp( argv[1], "-tree",5)==0)
+	{
+		float entropy =huffman.compute_entropy(img,huffman.getSymbols());
+		huffman.build_tree(*huffman.getSymbols());
+	}
+	else if (strncmp( argv[1],"-encode",7)==0 && argc<3)
+	{
+		float entropy =huffman.compute_entropy(img,huffman.getSymbols());
+		huffman.build_tree(*huffman.getSymbols());
+		int length = huffman.encode(img,&encodedMsg);
+		cout<<length<<endl;
+	}
+	else if(strncmp( argv[1],"-encode",7)==0 && argc ==3 )
+	{
+		decorrelation(width,height,&img);
+		float entropy =huffman.compute_entropy(img,huffman.getSymbols());
+		huffman.build_tree(*huffman.getSymbols());
+		float length = huffman.encode(img,&encodedMsg);
+		cout<<length;
+	}
+	return 0;
+}
+
+void readPgm(string& version,int& width ,int& height , int& range ,vector<unsigned char>* img ){
+	char x;
+	string test;
+	int val;
+	cin>>x;
+	version+=x;
+	cin>>x;
+	version+=x;
+	while(true)
+	{
+		cin>>test;
+		if(test[0]!='#')
+		{
+			break;
+		}
+		else
+		{
+			getline(cin,test);
+		}
+	}
+	width = stoi(test);
+	cin>>height;
+	cin>>range;
+	for(int i=0;i<width*height;i++)
+	{
+		cin>>val;
+		img->push_back(val);
+	}
+}
+
+void decorrelation(int width,int height,vector<unsigned char>*symbols)
+{
+	for(int i=0;i<height;i++)
+	{
+		for(int j=width-1;j>0;j--)
+		{
+			symbols->at(i*width+j) -= symbols->at(i*width+j-1) ;
+		}
+	}
+}
